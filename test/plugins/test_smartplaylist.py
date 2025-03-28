29584aba700c3b5697ef84f0a1fa39a74a9961af
ubuntu <ubuntu@ip-10-0-0-108>
2025-03-28 01:10:24 +0000

    waleko__o1_7e50__id_40

--- test/plugins/test_smartplaylist.py
+++ test/plugins/test_smartplaylist.py
@@ -283,6 +283,7 @@ class SmartPlaylistTest(_common.TestCase):
         self.assertEqual(content, b"http://beets:8337/item/3/file\n")
 
 
+
 class SmartPlaylistCLITest(_common.TestCase, TestHelper):
     def setUp(self):
         self.setup_beets()
